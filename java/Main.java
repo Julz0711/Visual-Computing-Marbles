4a00cf9c4f78c45159f0064913ceaa974a17ccb8
Julz <julian.gkt@gmail.com>
2022-03-29 22:54:29 +0200

    Init Push

---
+++ java/Main.java
@@ -0,0 +1,21 @@
+package Marbels;
+
+import java.util.Arrays;
+
+class Main {
+
+    //Kevin stinkt
+    public static void main(String[] args) {
+        double[] v = new double[] { 20, 10 };
+        double[] s = new double[] { 0, 0 };
+        double[] a = new double[] { 0, 9.81 };
+        Ball ball = new Ball(a, v, s);
+
+        for (int i = 0; i <= 29; i++) {
+            ball.nextFrame();
+            System.out.println("V: " + Arrays.toString(ball.getV()) +
+                    ", S: " + Arrays.toString(ball.getS()) +
+                    ", A: " + Arrays.toString(ball.getA()));
+        }
+    }
+}
